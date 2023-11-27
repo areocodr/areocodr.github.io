3e9058f1e1a556c3d0685e23863b96c1b2e25214
areocodr <jibo.areo@gmail.com>
2023-11-27 10:35:17 +0100

    added student forms

--- dashboard-script.js
+++ dashboard-script.js
@@ -53,3 +53,67 @@ window.addEventListener("resize", function () {
     searchForm.classList.remove("show");
   }
 });
+const students = [];
+
+function addStudent() {
+  const name = document.getElementById("name").value;
+  const course = document.getElementById("course").value;
+  const faculty = document.getElementById("faculty").value;
+  const gender = document.getElementById("gender").value;
+  const address = document.getElementById("address").value;
+  const religion = document.getElementById("religion").value;
+  const dob = document.getElementById("dob").value;
+  const state = document.getElementById("state").value;
+
+  const student = {
+    name,
+    course,
+    faculty,
+    gender,
+    address,
+    religion,
+    dob,
+    state,
+  };
+
+  students.push(student);
+
+  displayStudents();
+  clearForm();
+}
+
+function displayStudents() {
+  const table = document.getElementById("studentTable");
+  // Clear existing rows
+  table.innerHTML =
+    "<tr><th>Name</th><th>Course</th><th>Faculty</th><th>Gender</th><th>Home Address</th><th>Religion</th><th>Date of Birth</th><th>State of Origin</th><th>Action</th></tr>";
+
+  // Add new rows
+  students.forEach((student, index) => {
+    const row = table.insertRow(-1);
+    const cellCount = Object.keys(student).length + 1;
+
+    for (let i = 0; i < cellCount - 1; i++) {
+      const cell = row.insertCell(i);
+      cell.innerHTML = Object.values(student)[i];
+    }
+
+    // Add delete button
+    const deleteCell = row.insertCell(cellCount - 1);
+    const deleteButton = document.createElement("button");
+    deleteButton.innerHTML = "Delete";
+    deleteButton.onclick = function () {
+      deleteStudent(index);
+    };
+    deleteCell.appendChild(deleteButton);
+  });
+}
+
+function deleteStudent(index) {
+  students.splice(index, 1);
+  displayStudents();
+}
+
+function clearForm() {
+  document.getElementById("studentForm").reset();
+}
